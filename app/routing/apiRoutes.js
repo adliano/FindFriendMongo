6422d7e25cdf726d245e648910cdd710c40f5ae1
Adliano Alves <adliano@me.com>
2019-06-23 18:33:40 -0700

    fixed all routes and now using mongodb to save data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,44 +1,26 @@
 /**
- * Your `apiRoutes.js` file should contain two routes:
+ * The `apiRoutes.js` file contain two routes:
  * A GET route with the url `/api/friends`.
  * This will be used to display a JSON of all possible friends.
  * A POST routes `/api/friends`.
  * This will be used to handle incoming survey results.
  * This route will also be used to handle the compatibility logic.
  */
-
-// let friends = require('../data/friends')
 // Require Mongojs
 let mongojs = require('mongojs')
 // Mongo database config
 let databaseURL = 'friends'
+// Mongo collection(s) (like as "Tables" in SQL)
 let collections = ['users']
 // Init Mong db with congiguration
+// This will create the database and collection
 let db = mongojs(databaseURL, collections)
 
 // On Error event listener
-db.on('error', (error) => {
+db.on('error', error => {
   console.log('⚠️ ☣️ ☢️'.repeat(10))
   console.error(error)
 })
-
-
-
-
-
-function getAllFriends() {
-  db.users.find( (err, found) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-   else {
-     return found
-   }
-  })
-}
-
-
 /**************************************
  * Method findMatch()
  * @param {Array} usrScore
@@ -47,34 +29,39 @@ function getAllFriends() {
  * This Method will compares users scores and find the closest match
  **************************************/
 function findMatch (usrScore) {
-  // Return a Promise
-  let friends = getAllFriends()
-  console.log(friends)
   return new Promise((resolve, reject) => {
     // Max possible score will be 40
     let _matchedUser = { score: 40 }
-    resolve(friends)
-    //
-    // Clear console for better debug
-    // console.clear()
-    // Loop throught friends array to calculate results
-    // friends.forEach(function (item, usrIndex) {
-    //   let _results = 0
-
-    //   // console.log(item.score);
-    //   // Loop through score array
-    //   item.score.forEach(function (item, index) {
-    //     // Get Sum of Diferrence between current user score and saved user score
-    //     _results = _results + Math.abs(item - usrScore[index])
-    //   })
-    //   // Check if score its close to current user and get its index
-    //   if (_matchedUser.score > _results) {
-    //     _matchedUser.score = _results
-    //     _matchedUser.index = usrIndex
-    //   }
-    // })
-    // Pass the Mathed user
-    // resolve(friends[_matchedUser.index])
+    // Search for user on Mongodb
+    db.users.find((err, found) => {
+      // Check for error on database
+      if (err) {
+        console.error(err)
+        // In case of error reject
+        reject(err)
+      } else {
+        // Found will return a array with dababase object(s)
+        // Loop throught found array to calculate results and find match
+        found.forEach( (item, usrIndex) => {
+          console.log('Looking for a match....')
+          // Reset results to zero for each user
+          let _results = 0
+          // score its a array with numbers that users select on quetionary
+          // Loop through score array and compare to find the match
+          item.score.forEach( (item, index) => {
+            // Get Sum of Diferrence between current user score and saved user score
+            _results = _results + Math.abs(item - usrScore[index])
+          })
+          // Check if score its close to current user and get its index
+          if (_matchedUser.score > _results) {
+            _matchedUser.score = _results
+            _matchedUser.index = usrIndex
+          }
+        })
+        // Pass the Mathed user
+        resolve(found[_matchedUser.index])
+      }
+    })
   })
 }
 
@@ -85,11 +72,16 @@ function apiRoutes (app) {
    * GETs Routes
    *
    **************************************/
-  let friends = getAllFriends()
   app.get('/api/friends', function (req, res) {
-    res.send(friends)
+    db.users.find((err, found) => {
+      if (err) {
+        console.error(err)
+        res.send(err)
+      } else {
+        res.send(found)
+      }
+    })
   })
-
   /**************************************
    *
    * POSTs Routes
@@ -97,13 +89,13 @@ function apiRoutes (app) {
    **************************************/
   app.post('/api/friends', function (req, res) {
     // Call method findMath() to find Matched user
-    // findMatch(req.body.score).then(user => {
+    findMatch(req.body.score).then(user => {
       // then send the matched user to client
-      // res.json(user)
+      res.json(user)
       // and add it to friends array
       // friends.push(req.body)
       db.users.save(req.body)
-    // })
+    })
   })
 }
 
