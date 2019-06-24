cd43f295112e25bf3f21bbdc87c192148ed4f48a
Adliano Alves <adliano@me.com>
2019-06-23 17:21:15 -0700

    add db to routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,12 +7,12 @@
  * This route will also be used to handle the compatibility logic.
  */
 
-let friends = require('../data/friends')
+// let friends = require('../data/friends')
 // Require Mongojs
 let mongojs = require('mongojs')
 // Mongo database config
 let databaseURL = 'friends'
-let collections = 'users'
+let collections = ['users']
 // Init Mong db with congiguration
 let db = mongojs(databaseURL, collections)
 
@@ -21,6 +21,23 @@ db.on('error', (error) => {
   console.log('⚠️ ☣️ ☢️'.repeat(10))
   console.error(error)
 })
+
+
+
+
+
+function getAllFriends() {
+  db.users.find( (err, found) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+   else {
+     return found
+   }
+  })
+}
+
 
 /**************************************
  * Method findMatch()
@@ -31,30 +48,33 @@ db.on('error', (error) => {
  **************************************/
 function findMatch (usrScore) {
   // Return a Promise
+  let friends = getAllFriends()
+  console.log(friends)
   return new Promise((resolve, reject) => {
     // Max possible score will be 40
     let _matchedUser = { score: 40 }
+    resolve(friends)
     //
     // Clear console for better debug
-    console.clear()
+    // console.clear()
     // Loop throught friends array to calculate results
-    friends.forEach(function (item, usrIndex) {
-      let _results = 0
+    // friends.forEach(function (item, usrIndex) {
+    //   let _results = 0
 
-      // console.log(item.score);
-      // Loop through score array
-      item.score.forEach(function (item, index) {
-        // Get Sum of Diferrence between current user score and saved user score
-        _results = _results + Math.abs(item - usrScore[index])
-      })
-      // Check if score its close to current user and get its index
-      if (_matchedUser.score > _results) {
-        _matchedUser.score = _results
-        _matchedUser.index = usrIndex
-      }
-    })
+    //   // console.log(item.score);
+    //   // Loop through score array
+    //   item.score.forEach(function (item, index) {
+    //     // Get Sum of Diferrence between current user score and saved user score
+    //     _results = _results + Math.abs(item - usrScore[index])
+    //   })
+    //   // Check if score its close to current user and get its index
+    //   if (_matchedUser.score > _results) {
+    //     _matchedUser.score = _results
+    //     _matchedUser.index = usrIndex
+    //   }
+    // })
     // Pass the Mathed user
-    resolve(friends[_matchedUser.index])
+    // resolve(friends[_matchedUser.index])
   })
 }
 
@@ -65,6 +85,7 @@ function apiRoutes (app) {
    * GETs Routes
    *
    **************************************/
+  let friends = getAllFriends()
   app.get('/api/friends', function (req, res) {
     res.send(friends)
   })
@@ -76,12 +97,13 @@ function apiRoutes (app) {
    **************************************/
   app.post('/api/friends', function (req, res) {
     // Call method findMath() to find Matched user
-    findMatch(req.body.score).then(user => {
+    // findMatch(req.body.score).then(user => {
       // then send the matched user to client
-      res.json(user)
+      // res.json(user)
       // and add it to friends array
-      friends.push(req.body)
-    })
+      // friends.push(req.body)
+      db.users.save(req.body)
+    // })
   })
 }
 
